9c03d0f65cb746fa0db01e6ddc4b9054d7e1584f
emma-jinger <73496405+emma-jinger@users.noreply.github.com>
2022-07-11 17:42:18 -0700

    Update simple_etl.py

--- simple_etl.py
+++ simple_etl.py
@@ -1,1 +1,26 @@
-#Hello
+import logging
+
+LOGGER = logging.getLogger("ETL")
+LOGGER.setLevel(logging.INFO)
+
+LOGGER.propagate = False
+# create formatter
+formatter = logging.Formatter("%(asctime)s :%(levelname)s: %(message)s")
+
+
+# output on stdout
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+
+# add formatter to stream_handler
+stream_handler.setFormatter(formatter)
+LOGGER.addHandler(stream_handler)
+
+# also try to output to log file (may consider try and except later like kernel.test.schema)
+file_handler = logging.FileHandler(filename="/var/log/ETL.log")
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+LOGGER.addHandler(file_handler)
+
