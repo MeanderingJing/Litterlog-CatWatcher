38aa859ebfe31d10495a6940d77a6d75bc681bf8
cat_dev <emma.lijing0723@gmail.com>
2022-07-29 15:37:48 -0700

    new file:   CatDataSchema/MANIFEST.in 	modified:   CatDataSchema/setup.py 	CatDataSchema/VERSION 	CatDataSchema/requirements.txt

--- CatDataSchema/setup.py
+++ CatDataSchema/setup.py
@@ -7,24 +7,23 @@ with open("VERSION", "r") as version_file:
     version = version_file.read()
 
 setup(
-    name="kernel.test.schema",
+    name="CatDataSchema",
     version=version,
-    author="kernel",
-    description="test schema package",
-    url="https://git.kernel.corp/projects/MNT/repos/test-station-schema/browse",
+    author="Emma Li",
+    description="cat data schema package",
+    url="https://github.com/emma-jinger/CatWatcher/tree/main/CatDataSchema",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_namespace_packages(include=["kernel.test.schema", "kernel.test.schema.*"]),
+    packages=find_namespace_packages(include=["CatDataSchema", "CatDataSchema.*"]),
     include_package_data=True,
-    install_requires=open("requirements.txt").read().splitlines()[2:],
+    install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
         "console_scripts": [
-            "test_data_watcher=kernel.test.schema.cli:test_data_watcher",
-            "test_result_watcher=kernel.test.schema.cli:test_result_watcher",
-            "kts-migrate=kernel.test.schema.cli:migrate",
+            "cat_data_watcher=CatDataSchema.cli:cat_data_watcher",
+            "cat_data_migrate=CatDataSchema.cli:migrate",
         ]
     },
-    package_data={"kernel.test.schema": []},
+    package_data={"CatDataSchema": []},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
